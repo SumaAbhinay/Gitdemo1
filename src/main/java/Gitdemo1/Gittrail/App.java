4d60b563714f2cb31392c882451616bfdb05be69
suma <sumangowda961772@gmail.com>
2022-03-25 17:34:18 +0530

    first commit

---
+++ src/main/java/Gitdemo1/Gittrail/App.java
@@ -0,0 +1,13 @@
+package Gitdemo1.Gittrail;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Gitdemo1/Gittrail/AppTest.java
@@ -0,0 +1,20 @@
+package Gitdemo1.Gittrail;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
